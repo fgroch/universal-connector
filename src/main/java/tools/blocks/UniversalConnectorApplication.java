d42dfeba61f00b3bfd22d8bf142ccc9c1e56a504
Filip Grochowski <fgroch@gmail.com>
2018-09-18 00:06:36 +0200

    Project creation (#1)

---
+++ src/main/java/tools/blocks/UniversalConnectorApplication.java
@@ -0,0 +1,12 @@
+package tools.blocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversalConnectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversalConnectorApplication.class, args);
+    }
+}
---
+++ src/test/java/tools/blocks/UniversalConnectorApplicationTests.java
@@ -0,0 +1,16 @@
+package tools.blocks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UniversalConnectorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
